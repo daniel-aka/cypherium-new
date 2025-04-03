bba9e7d228a68fc79b96dd9110dd314f67a3b8dd
Akachukwu Daniel <akachukwudaniel37@gmail.com>
2025-04-03 14:19:55 +0100

    Responsiveness updated 4

--- backend/controllers/investmentController.js
+++ backend/controllers/investmentController.js
@@ -8,7 +8,7 @@ exports.createInvestment = async (req, res) => {
         const userId = req.user._id;
 
         // Validate plan type
-        if (!['Gold Plan', 'Silver Plan', 'Bronze Plan'].includes(planType)) {
+        if (!['basic', 'premium', 'vip'].includes(planType)) {
             return res.status(400).json({ message: 'Invalid plan type' });
         }
 
